5b6285025b84ecbf66ebb27fa8524d2da50d2610
padraic-edwards <padraic.edwards@ie.ibm.com>
2017-04-23 22:10:40 +0100

    update profiles example

--- 20465981/mead3986/static/samples/profiles/profilesCustomization.js
+++ 20465981/mead3986/static/samples/profiles/profilesCustomization.js
@@ -6,7 +6,7 @@
 //
 if(typeof(dojo) != "undefined") {
     dojo.place(
-        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/profiles/profilesCustomization.css\"></link>",
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/samples/profiles/profilesCustomization.css\"></link>",
         dojo.doc.head,
         "last"
     );
