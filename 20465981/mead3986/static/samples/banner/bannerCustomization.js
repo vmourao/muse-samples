6a288981dbc0fd26fbb10e19b50f42bc1e4cfb61
padraic-edwards <padraic.edwards@ie.ibm.com>
2017-04-25 16:33:23 +0100

    add banner example

---
+++ 20465981/mead3986/static/samples/banner/bannerCustomization.js
@@ -0,0 +1,13 @@
+////
+// @author Padraic Edwards, IBM
+// @name Banner Customization
+// @version 0.1
+// @date February, 2017
+//
+if(typeof(dojo) != "undefined") {
+    dojo.place(
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/samples/banner/bannerCustomization.css\"></link>",
+        dojo.doc.head,
+        "last"
+    );
+}
