d102ba87fe3b85a46ca2df5c12445a7a11467346
padraic-edwards <padraic.edwards@ie.ibm.com>
2017-04-26 10:04:49 +0100

    update sample

--- 20465981/mead3986/static/samples/flipCard/commListCardsFlipStyle.user.js
+++ 20465981/mead3986/static/samples/flipCard/commListCardsFlipStyle.user.js
@@ -5,13 +5,13 @@
 //
 // Non-Disclosure Agreement
 // Sharing of this functionality outside of this meeting is prohibited. The content of this prototype is meant to be shared only with IBM Employees or parties
-// under NDA with IBM. IBM’s statements regarding its plans, directions, and intent are subject to change or withdrawal without notice at IBM’s sole discretion.
+// under NDA with IBM. IBMâ€™s statements regarding its plans, directions, and intent are subject to change or withdrawal without notice at IBMâ€™s sole discretion.
 // Information regarding potential future products is intended to outline our general product direction and it should not be relied on in making a purchasing decision.
 // The information mentioned regarding potential future products is not a commitment, promise, or legal obligation to deliver any material, code or functionality. 
 // Information about potential future products may not be incorporated into any contract. The development, release, and timing of any future features or functionality 
 // described for our products remains at our sole discretion. Performance is based on measurements and projections using standard IBM benchmarks in a controlled environment.
 // The actual throughput or performance that any user will experience will vary depending upon many factors, including considerations such as the amount of multiprogramming 
-// in the user’s job stream, the I/O configuration, the storage configuration, and the workload processed. Therefore, no assurance can be given that an individual
+// in the userâ€™s job stream, the I/O configuration, the storage configuration, and the workload processed. Therefore, no assurance can be given that an individual
 // user will achieve results similar to those stated here.
 //
 // @name         commListCardsFlipStyle
@@ -166,7 +166,6 @@ if(typeof(dojo) != "undefined") {
             // connect table content changes to recreate the tiles
             dojo.subscribe(lconn.communities.catalog.DISPLAY_RESULTS_TOPIC, function(){ createCommTiles(); });
             
-			
             // grid vs list selector
             dojo.place('<div id="viewControl" class="lotusViewControl lotusRight">'+
               	    	   '<a id="viewControlTable" class="lotusSprite lotusView lotusDetailsOn" href="javascript:;" onclick="showTable();"><span class="lotusAltText ">Customizable</span></a>'+
