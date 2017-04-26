f3dea4e90423082a27bc23ba471b136f6f0c66b2
cathalcu@ie.ibm.com <cathalcu@ie.ibm.com>
2017-04-26 15:45:54 +0100

    SampleTest

---
+++ 1000000176/suzanne3101/homepagerWatsonWorkspaceStatus.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         WatsonWorkspace - Status
+// @namespace    WatsonWorkspace-Status
+// @version      0.1
+// @description  Post Connections Status to WatsonWorkspace!
+// @author       You
+// @match        https://apps.na.collabserv.com/homepage/web/updates/*
+// @grant          GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+    if(!dojo) {
+        return;
+    }
+
+    const _logger = data => console.log('****** ', data, ' ********');
+
+    const onCheck = () => {
+        const email = gllConnectionsData.caller;
+        _logger(email);
+        const editor = dijit.byId('lconn_news_microblogging_sharebox_InputForm_0_textBoxContainer');
+        _logger(editor);
+        const button = dijit.byId('lconn_news_microblogging_sharebox_InputForm_0_myactions');
+        _logger(button);
+        if(!email || !editor || !button) {
+            setTimeout(onCheck, 1000);
+            return;
+        }
+        dojo.connect(button.postButtonNode, "onclick", e => {
+            _logger('CLICKED');
+            const text = editor.getText();
+            if(text && text.length) {
+                const data = JSON.stringify({ email, text });
+                _logger(data);
+                GM_xmlhttpRequest({
+                    method: 'POST',
+                    url: 'https://489ea395.ngrok.io/webhook1',
+                    data,
+                    headers: {
+                        'Content-Type' : 'application/json; charset=UTF-8'
+                    },
+                    onload: res => _logger(res)
+                });
+            }
+        });
+    };
+
+    dojo.addOnLoad(onCheck);
+})();
