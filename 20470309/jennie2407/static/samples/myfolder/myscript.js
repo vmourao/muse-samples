8ecac90aadbd9d9d487209053add26ea259b3ab3
jsnampalli <jayasree_nampalli@us.ibm.com>
2017-04-27 13:24:52 -0500

    Update myscript.js

--- 20470309/jennie2407/static/samples/myfolder/myscript.js
+++ 20470309/jennie2407/static/samples/myfolder/myscript.js
@@ -1,1 +1,8 @@
 
+if(typeof(dojo) != "undefined") {
+    dojo.place(
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/samples/FilesImg/FilesImg.css\"></link>",
+        dojo.doc.head,
+        "last"
+    );
+}
