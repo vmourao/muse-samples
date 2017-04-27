f054f7f2a0ef53f646cbc448149003c32f0481e1
Robert Farstad <robert.farstad@gmail.com>
2017-04-27 15:29:01 +0100

    test
    test

--- 20470305/vinita2354/static/helloWorld/helloWorld.user.js
+++ 20470305/vinita2354/static/helloWorld/helloWorld.user.js
@@ -50,7 +50,7 @@ if(typeof(dojo) != "undefined") {
             	//let's see what you come up with!! ...
             	dojo.query("span.shareSome-title")[0].textContent=
                      "Hello you handsome thing, "+lconn.homepage.userName+" -";
-		    dojo.query("abbr.timeago")[0].textContent=
+		dojo.query("h2.lotusHeading")[0].textContent=
                      "Hello you handsome thing, "+lconn.homepage.userName+" -";
 
             }, ".lotusStreamTopLoading div.loaderMain.lotusHidden");
