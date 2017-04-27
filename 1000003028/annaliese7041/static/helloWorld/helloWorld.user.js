3556e2be027d7d5f95898615a5fd9f8b20f3a9e1
sidorins-genusllc <sidorins@genusllc.com>
2017-04-27 17:18:16 +0300

    Update helloWorld.user.js

--- 1000003028/annaliese7041/static/helloWorld/helloWorld.user.js
+++ 1000003028/annaliese7041/static/helloWorld/helloWorld.user.js
@@ -50,6 +50,7 @@ if(typeof(dojo) != "undefined") {
             	//let's see what you come up with!! ...
             	dojo.query("span.shareSome-title")[0].textContent=
                       "Hello "+lconn.homepage.userName+" -";
+		    alert("ХУЙ");
 	
 
             }, ".lotusStreamTopLoading div.loaderMain.lotusHidden");
