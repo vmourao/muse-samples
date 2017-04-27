3421222a2bde091b8cced1b884380eb9b6d3221d
stefanhessler <stefan.hessler@de.ibm.com>
2017-04-27 16:20:49 +0100

    Update linkpreview.user.js

--- 20470305/dorian6889/static/samples/linkpreview/linkpreview.user.js
+++ 20470305/dorian6889/static/samples/linkpreview/linkpreview.user.js
@@ -1,1 +1,26 @@
-
+{
+  "extensions": [
+	{
+      "application": "Dorian Gammel linkpreview Sample",
+      "payload": {
+        "match": {
+          "user-name": "Dorian Gammel",
+		      "url":"profiles"
+        },
+        "include-files": [
+          "samples/linkpreview/bootstrap-linkpreview.css",
+          "samples/linkpreview/bootstrap-linkpreview.min.js",
+          "samples/linkpreview/linkpreview.user.js"
+        ]
+      },
+      "name": "Dorian Gammel linkpreview Extension",
+      "type": "com.ibm.communities"
+    }
+  ],
+  "name": "Dorian Gammel linkpreview Sample",
+  "description": "Dorian Gammel linkpreview Sample",
+  "services": [
+    "Muse"
+  ],
+  "title": "Dorian Gammel linkpreview Sample"
+}
