c1d15721773ba25d4449c17e7c4a0bc777347dbf
hwschauer <herwig.schauer@at.ibm.com>
2017-04-27 15:49:38 +0200

    Update helloWorld.user.js

--- 20470311/christa5308/static/helloWorld/helloWorld.user.js
+++ 20470311/christa5308/static/helloWorld/helloWorld.user.js
@@ -48,6 +48,9 @@ if(typeof(dojo) != "undefined") {
             	//indicating that we have loaded content.
                 //more of your own customization code here please :-) 
             	//let's see what you come up with!! ...
+		    
+		    dojo.query("span.shareSome-title")[0].textContent=
+                      "Hail you,my godlike and handsome "+lconn.homepage.userName+" -";
             	
 
             }, ".lotusStreamTopLoading div.loaderMain.lotusHidden");
