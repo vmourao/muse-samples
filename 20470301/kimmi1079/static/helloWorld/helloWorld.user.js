1f4138363cfee380bb52f12ce8c615ecb9776757
NielsBoumans <nboumans@acuity.nl>
2017-04-27 16:29:18 +0200

    Update helloWorld.user.js

--- 20470301/kimmi1079/static/helloWorld/helloWorld.user.js
+++ 20470301/kimmi1079/static/helloWorld/helloWorld.user.js
@@ -49,7 +49,7 @@ if(typeof(dojo) != "undefined") {
                 //more of your own customization code here please :-) 
             	//let's see what you come up with!! ...
             	
-
+		dojo.query("#hpUserPhoto").style("border", "2px solid pink");
             }, ".lotusStreamTopLoading div.loaderMain.lotusHidden");
 
       } catch(e) {
