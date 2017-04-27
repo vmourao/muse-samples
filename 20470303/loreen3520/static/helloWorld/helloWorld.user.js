bf3cd2a7300f63858bb1bf5ee2b5dd32c5eee810
David Simpson <david@davidsimpson.me>
2017-04-27 14:52:39 +0100

    Update helloWorld.user.js

--- 20470303/loreen3520/static/helloWorld/helloWorld.user.js
+++ 20470303/loreen3520/static/helloWorld/helloWorld.user.js
@@ -49,7 +49,7 @@ if(typeof(dojo) != "undefined") {
                 //more of your own customization code here please :-) 
             	//let's see what you come up with!! ...
 		
-            	dojo.query("div.ics-scbanner")[0].style = 'background: hotpink!important;';
+            	//dojo.query("div.ics-scbanner")[0].style = 'background: hotpink!important;';
 		dojo.query("span.shareSome-title")[0].textContent=
                      "Hello "+lconn.homepage.userName+" -";    
             	
