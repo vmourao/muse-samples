281bfacbc3d2d8352d2957be3caaa3ece65ae7df
Ted Hardenburgh <thardenb@gmail.com>
2017-04-27 07:10:21 -0700

    Update helloWorld.user.js

--- 20470309/kali3064/static/helloWorld/helloWorld.user.js
+++ 20470309/kali3064/static/helloWorld/helloWorld.user.js
@@ -49,7 +49,7 @@ if(typeof(dojo) != "undefined") {
                 //more of your own customization code here please :-) 
             	//let's see what you come up with!! ...
             	dojo.query("span.shareSome-title")[0].textContent=
-                     "Hello "+lconn.homepage.userName+" -";
+                     "Hello there "+lconn.homepage.userName+" -";
 
             }, ".lotusStreamTopLoading div.loaderMain.lotusHidden");
 
