608291b29b6bbee2032783fc24dc763fd1f51b62
martdj <mdejong@ilionx.com>
2017-04-27 17:19:52 +0200

    Create FilesScript.js

---
+++ 20470303/vanny2052/static/samples/FilesImg/FilesScript.js
@@ -0,0 +1,7 @@
+if(typeof(dojo) != "undefined") {
+    dojo.place(
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/samples/FilesImg/FilesImg.css\"></link>",
+        dojo.doc.head,
+        "last"
+    );
+}
