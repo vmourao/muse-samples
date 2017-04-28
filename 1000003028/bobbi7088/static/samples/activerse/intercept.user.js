7321e56d29b644895a2e259231591a2d7903d0b2
Daniele Vistalli <daniele.vistalli@factor-y.com>
2017-04-28 10:30:08 +0200

    Update intercept.user.js

--- 1000003028/bobbi7088/static/samples/activerse/intercept.user.js
+++ 1000003028/bobbi7088/static/samples/activerse/intercept.user.js
@@ -1,1 +1,12 @@
-console.log('Activities - Interceptor')
+
+(function() {
+  // URL Inteceptor + redirector
+
+  console.log('Activities - Interceptor')
+  targetUrl = "/files/muse-static/samples/activerse/index_vue.html" + location.search + location.hash;
+  
+  console.log("Replacing activities with: " + targeturl);
+  
+  location.replace(targetUrl);
+  
+})();
