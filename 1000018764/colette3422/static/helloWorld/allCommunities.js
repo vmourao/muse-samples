43139d7ef76c869c5815e109c4fe620c30378736
padraic-edwards <padraic.edwards@ie.ibm.com>
2017-05-24 18:15:16 +0100

    update

--- 1000018764/colette3422/static/helloWorld/allCommunities.js
+++ 1000018764/colette3422/static/helloWorld/allCommunities.js
@@ -20,36 +20,14 @@
 if(typeof(dojo) != "undefined") {
 	require(["dojo/domReady!"], function(){
         try {
-            // utility function to let us wait for a specific element of the page to load...
-            var waitFor = function(callback, elXpath, elXpathRoot, maxInter, waitTime) {
-                if(!elXpathRoot) var elXpathRoot = dojo.body();
-                if(!maxInter) var maxInter = 10000;  // number of intervals before expiring
-                if(!waitTime) var waitTime = 1;  // 1000=1 second
-                if(!elXpath) return;
-                var waitInter = 0;  // current interval
-                var intId = setInterval( function(){
-                    if( ++waitInter<maxInter && !dojo.query(elXpath,elXpathRoot).length) return;
 
-                    clearInterval(intId);
-                    if( waitInter >= maxInter) { 
-                        console.log("**** WAITFOR ["+elXpath+"] WATCH EXPIRED!!! interval "+waitInter+" (max:"+maxInter+")");
-                    } else {
-                        console.log("**** WAITFOR ["+elXpath+"] WATCH TRIPPED AT interval "+waitInter+" (max:"+maxInter+")");
-                        callback();
-                    }
-                }, waitTime);
-            };
-
-            // here we use waitFor to wait on the .lotusStreamTopLoading div.loaderMain.lotusHidden element
-            // before we proceed to customize the page...
-            waitFor( function(){
                 
             	//wait until the "loading..." node has been hidden, 
             	//indicating that we have loaded content. 
                 dojo.query(".lotusMeta")[4].textContent="This community can have members from outside your organization.";
 				dojo.query(".lotusMeta")[4].style="color:#6eff00";
 
-            }, "tr class='lotusFirst'");
+
 
       } catch(e) {
           alert("Exception occurred in helloWorld: " + e);
