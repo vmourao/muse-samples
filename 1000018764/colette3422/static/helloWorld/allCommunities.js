443d12675a51d13f38bd6152bcacb4acd5a14c3c
padraic-edwards <padraic.edwards@ie.ibm.com>
2017-05-24 18:20:03 +0100

    update for lotus meta

--- 1000018764/colette3422/static/helloWorld/allCommunities.js
+++ 1000018764/colette3422/static/helloWorld/allCommunities.js
@@ -49,7 +49,7 @@ if(typeof(dojo) != "undefined") {
                 dojo.query(".lotusMeta")[4].textContent="This community can have members from outside your organization.";
 				dojo.query(".lotusMeta")[4].style="color:#6eff00";
 
-            }, "tr div[class='lotusFirst']");
+            }, "tr div[class='lotusMeta']");
 
       } catch(e) {
           alert("Exception occurred in helloWorld: " + e);
