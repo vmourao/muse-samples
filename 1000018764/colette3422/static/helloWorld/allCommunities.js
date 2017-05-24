4dc371b599d907ac476f131aba4ce35d501d0b0e
padraic-edwards <padraic.edwards@ie.ibm.com>
2017-05-24 23:20:12 +0100

    update wait for

--- 1000018764/colette3422/static/helloWorld/allCommunities.js
+++ 1000018764/colette3422/static/helloWorld/allCommunities.js
@@ -21,25 +21,18 @@ if(typeof(dojo) != "undefined") {
 	require(["dojo/domReady!"], function(){
         try {
             // utility function to let us wait for a specific element of the page to load...
-            var waitFor = function(callback, elXpath, elXpathRoot, maxInter, waitTime) {
-                if(!elXpathRoot) var elXpathRoot = dojo.body();
-                if(!maxInter) var maxInter = 10000;  // number of intervals before expiring
-                if(!waitTime) var waitTime = 1;  // 1000=1 second
-                if(!elXpath) return;
-                var waitInter = 0;  // current interval
-                var intId = setInterval( function(){
-					console.log("xpath "+dojo.query(elXpath,elXpathRoot).length);
-                    if( ++waitInter<maxInter && !dojo.query(elXpath,elXpathRoot).length) return;
-
-                    clearInterval(intId);
-                    if( waitInter >= maxInter) { 
-                        console.log("**** WAITFOR ["+elXpath+"] WATCH EXPIRED!!! interval "+waitInter+" (max:"+maxInter+")");
-                    } else {
-                        console.log("**** WAITFOR ["+elXpath+"] WATCH TRIPPED AT interval "+waitInter+" (max:"+maxInter+")");
-                        callback();
-                    }
-                }, waitTime);
-            };
+		var waitFor = function(callback, elXpath, maxInter, waitTime) {
+        if(!maxInter) var maxInter = 20;  // number of intervals before expiring
+        if(!waitTime) var waitTime = 100;  // 1000=1 second
+        if(!elXpath) return;
+        
+        var waitInter = 0;  // current interval
+        var intId = setInterval(function(){
+            if (++waitInter<maxInter && !dojo.query(elXpath).length) return;
+            clearInterval(intId);
+            callback();
+        }, waitTime);
+		};
 
             // here we use waitFor to wait on the .lotusStreamTopLoading div.loaderMain.lotusHidden element
             // before we proceed to customize the page...
